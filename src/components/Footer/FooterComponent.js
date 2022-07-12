c803d4b2877ec39cbab707ed90bef7b90c4970fc
naluwan <street.baller06@gmail.com>
2022-07-13 00:32:04 +0800

    feat: add FooterComponent and style.css

---
+++ src/components/Footer/FooterComponent.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+const FooterComponent = () => {
+  return (
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col">LOGO</div>
+        <div className="col">客戶服務</div>
+        <div className="col">關於我們</div>
+        <div className="col">資訊</div>
+        <div className="col">追蹤ALPHA Shop</div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterComponent;
