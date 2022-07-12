b2c3a50570726df942fc704fbbb9a96080e6c0c9
naluwan <street.baller06@gmail.com>
2022-07-13 00:29:35 +0800

    feat: uninstall reactstrap and install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import FooterComponent from 'components/Footer/FooterComponent';
 import HeaderComponent from './components/Header/HeaderComponent';
 
 const App = () => {
   return (
     <div className="App">
       <HeaderComponent />
+      <FooterComponent />
     </div>
   );
 };
