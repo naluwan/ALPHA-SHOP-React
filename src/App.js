8a64748974ffb33317a69109e0bda5995b2c1f36
naluwan <street.baller06@gmail.com>
2022-07-12 23:33:41 +0800

    feat: init project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HeaderComponent from './components/Header/HeaderComponent';
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeaderComponent />
+    </div>
+  );
+};
+
+export default App;
