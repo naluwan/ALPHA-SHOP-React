f7e412d906163c2ead88688cf1c5bbcbac62e621
naluwan <street.baller06@gmail.com>
2022-07-14 00:14:43 +0800

    feat: update FooterComponent JSX and style.css

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -2,13 +2,75 @@ import './style.css';
 
 const FooterComponent = () => {
   return (
-    <footer className="container pt-5">
-      <div className="row align-items-center">
-        <div className="col">LOGO</div>
-        <div className="col">客戶服務</div>
-        <div className="col">關於我們</div>
-        <div className="col">資訊</div>
-        <div className="col">追蹤ALPHA Shop</div>
+    <footer className="site-footer">
+      <div className="footer-container">
+        <div className="footer-logo-container">
+          <svg className="icon-logo">
+            <use xlinkHref="#svg-icon-logo" />
+          </svg>
+        </div>
+        <section className="footer-section">
+          <h2 className="section-title">客戶服務</h2>
+          <div className="section-content">
+            <a className="page-link" href="/">
+              運送說明
+            </a>
+            <a className="page-link" href="/">
+              退換貨相關
+            </a>
+            <a className="page-link" href="/">
+              付款資訊
+            </a>
+            <a className="page-link" href="/">
+              FAQ
+            </a>
+          </div>
+        </section>
+        <section className="footer-section">
+          <h2 className="section-title">關於我們</h2>
+          <div className="section-content">
+            <a className="page-link" href="/">
+              品牌故事
+            </a>
+            <a className="page-link" href="/">
+              媒體聯繫
+            </a>
+            <a className="page-link" href="/">
+              Press kit
+            </a>
+          </div>
+        </section>
+        <section className="footer-section">
+          <h2 className="section-title">資訊</h2>
+          <div className="section-content">
+            <a className="page-link" href="/">
+              隱私權政策
+            </a>
+            <a className="page-link" href="/">
+              Cookie
+            </a>
+            <a className="page-link" href="/">
+              GDPR
+            </a>
+          </div>
+        </section>
+        <section className="footer-section">
+          <h2 className="section-title">追蹤 ALPHA Shop</h2>
+          <div className="section-content">
+            <div className="tel-info">+886 02123-45678</div>
+            <div className="social-icon-group">
+              <svg className="social-icon cursor-point">
+                <use xlinkHref="#svg-icon-facebook" />
+              </svg>
+              <svg className="social-icon cursor-point">
+                <use xlinkHref="#svg-icon-instagram" />
+              </svg>
+              <svg className="social-icon cursor-point">
+                <use xlinkHref="#svg-icon-whatsapp" />
+              </svg>
+            </div>
+          </div>
+        </section>
       </div>
     </footer>
   );
