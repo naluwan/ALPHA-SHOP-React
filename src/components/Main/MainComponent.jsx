5a1eb85950dc3f5ed26f71c282e739e28cb52804
naluwan <street.baller06@gmail.com>
2022-07-14 22:23:25 +0800

    feat: add React Hook useState - step and setStep and set them to props for ProgressControl

--- src/components/Main/MainComponent.jsx
+++ src/components/Main/MainComponent.jsx
@@ -1,11 +1,14 @@
 import StepProgress from 'components/StepProgress/StepProgress';
-// import Step1 from 'components/Step1/Step1';
-// import Step2 from 'components/Step2/Step2';
+import Step1 from 'components/Step1/Step1';
+import Step2 from 'components/Step2/Step2';
 import Step3 from 'components/Step3/Step3';
 import ProgressControl from 'components/ProgressControl/ProgressControl';
 import './style.css';
+import { useState } from 'react';
 
 const MainComponent = () => {
+  const [step, setStep] = useState('step1');
+
   return (
     <main className="site-main">
       <div className="main-container">
@@ -16,11 +19,11 @@ const MainComponent = () => {
         >
           <StepProgress />
           <section className="form-container col col-12 pt-5">
-            {/* <Step1 /> */}
-            {/* <Step2 /> */}
-            <Step3 />
+            {step === 'step1' && <Step1 />}
+            {step === 'step2' && <Step2 />}
+            {step === 'step3' && <Step3 />}
           </section>
-          <ProgressControl />
+          <ProgressControl onStep={step} onSetStep={setStep} />
         </section>
         <section className="col section-right">
           <h1>section right</h1>
