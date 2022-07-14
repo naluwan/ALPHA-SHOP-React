7672ed3042c046d97fa0dcd8ac2a3d8083cdaec4
naluwan <street.baller06@gmail.com>
2022-07-15 02:21:53 +0800

    feat: add 商品數量及價錢計算funciton

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,16 +1,18 @@
 import './style.css';
 import LineItem from 'components/LineItem/LineItem';
+import { useState } from 'react';
 import product1 from '../../assets/images/product1.jpeg';
 import product2 from '../../assets/images/product2.jpeg';
 
 const Cart = () => {
-  const dummyData = [
+  const productData = [
     {
       id: 1,
       img: product1,
       productName: '破壞補丁修身牛仔褲',
       productCount: 1,
       productPrice: 3999,
+      totalPrice: 0,
     },
     {
       id: 2,
@@ -18,21 +20,80 @@ const Cart = () => {
       productName: '刷色直筒牛仔褲',
       productCount: 1,
       productPrice: 1299,
+      totalPrice: 0,
     },
   ];
 
-  const LineItems = dummyData.map((product) => {
-    const { id, img, productName, productCount, productPrice } = product;
+  const [products, setProducts] = useState(productData);
+  // const [cartPrice, setCartPrice] = useState(() => {
+  //   let price = 0;
+  //   price = products.map((product) => product.productPrice + price);
+  // });
+  // console.log(cartPrice);
+  const plusBtnAtClick = (id) => {
+    setProducts((prevProducts) => {
+      const newProducts = [];
+      for (let i = 0; i < prevProducts.length; i += 1) {
+        const currentProduct = prevProducts[i];
+        if (currentProduct.id === id) {
+          let updateProduct = {
+            ...currentProduct,
+            productCount: currentProduct.productCount + 1,
+          };
+          updateProduct = {
+            ...updateProduct,
+            totalPrice: updateProduct.productCount * updateProduct.productPrice,
+          };
+          newProducts.push(updateProduct);
+          // TODO: 算出總價
+        } else {
+          newProducts.push(currentProduct);
+        }
+      }
+      return newProducts;
+    });
+  };
+
+  const minusBtnAtClick = (id) => {
+    setProducts((prevProducts) => {
+      const newProducts = [];
+      for (let i = 0; i < prevProducts.length; i += 1) {
+        const currentProduct = prevProducts[i];
+        if (currentProduct.id === id) {
+          let updateProduct = {
+            ...currentProduct,
+            productCount: currentProduct.productCount - 1,
+          };
+          updateProduct = {
+            ...updateProduct,
+            totalPrice: updateProduct.productCount * updateProduct.productPrice,
+          };
+          newProducts.push(updateProduct);
+        } else {
+          newProducts.push(currentProduct);
+        }
+      }
+      return newProducts;
+    });
+  };
+  const LineItems = products.map((product) => {
+    const { id, img, productName, productCount, productPrice, totalPrice } =
+      product;
     return (
       <LineItem
         key={id}
+        id={id}
         img={img}
         productName={productName}
         productCount={productCount}
         productPrice={productPrice}
+        totalPrice={totalPrice}
+        plusBtnAtClick={plusBtnAtClick}
+        minusBtnAtClick={minusBtnAtClick}
       />
     );
   });
+
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
@@ -45,7 +106,7 @@ const Cart = () => {
       </section>
       <section className="cart-info total col col-12">
         <div className="text">小計</div>
-        <div className="price" />
+        <div className="price">{123}</div>
       </section>
     </section>
   );
--- src/components/LineItem/LineItem.jsx
+++ src/components/LineItem/LineItem.jsx
@@ -1,28 +1,41 @@
 import './style.css';
 
 const LineItem = (props) => {
-  const { img, productName, productCount, productPrice } = props;
+  const {
+    id,
+    img,
+    productName,
+    productCount,
+    productPrice,
+    totalPrice,
+    plusBtnAtClick,
+    minusBtnAtClick,
+  } = props;
+
+  const currentPrice = totalPrice === 0 ? productPrice : totalPrice;
   return (
-    <div
-      className="product-container col col-12"
-      data-count="0"
-      data-price="3999"
-    >
+    <div className="product-container col col-12">
       <img className="img-container" src={img} alt="product-img" />
       <div className="product-info">
         <div className="product-name">{productName}</div>
         <div className="product-control-container">
           <div className="product-control">
-            <button className="product-action btn btn-secondary">
+            <button
+              className="product-action btn btn-secondary"
+              onClick={() => minusBtnAtClick(id)}
+            >
               <span className="minus">-</span>
             </button>
             <span className="product-count">{productCount}</span>
-            <button className="product-action btn btn-secondary">
+            <button
+              className="product-action btn btn-secondary"
+              onClick={() => plusBtnAtClick(id)}
+            >
               <span className="plus">+</span>
             </button>
           </div>
         </div>
-        <div className="price">{productPrice}</div>
+        <div className="price">{currentPrice}</div>
       </div>
     </div>
   );
