0d7497d4074c0fec3d08b0b3fe574125e555f0d0
naluwan <street.baller06@gmail.com>
2022-07-18 23:04:37 +0800

    feat: add nextBtn & prevBtn function

---
+++ src/components/ProgressControl/index.jsx
@@ -0,0 +1,37 @@
+import './style.css';
+
+const ProgressControl = (props) => {
+  const { onStep, onSetStep, stepMapLength } = props;
+
+  const nextBtnAtClick = () => {
+    onSetStep((prevStep) => prevStep + 1);
+  };
+
+  const prevBtnAtClick = () => {
+    onSetStep((prevStep) => prevStep - 1);
+  };
+
+  return (
+    <section className="progress-control-container col-12 pt-5">
+      <section className="button-group col-12" data-phase="credit-card">
+        <button
+          className={`prev ${onStep === 0 ? 'noShow' : ''}`}
+          onClick={prevBtnAtClick}
+        >
+          &larr; 上一步
+        </button>
+        <div>
+          {onStep !== stepMapLength - 1 ? (
+            <button className="next btn btn-lg" onClick={nextBtnAtClick}>
+              下一步 &rarr;
+            </button>
+          ) : (
+            <button className="next btn btn-lg">確認下單</button>
+          )}
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default ProgressControl;
