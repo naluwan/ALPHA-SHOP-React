9751f0f5ef93666d35030f493dd7c92425f6036f
naluwan <street.baller06@gmail.com>
2022-07-28 22:12:30 +0800

    feat: add React.memo and set the type of props

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,105 +1,33 @@
 import './style.css';
 import LineItem from 'components/LineItem';
-import { useState } from 'react';
-import product1 from '../../assets/images/product1.jpeg';
-import product2 from '../../assets/images/product2.jpeg';
+import React, { memo } from 'react';
 
-const Cart = () => {
-  const productData = [
-    {
-      id: 1,
-      img: product1,
-      productName: '破壞補丁修身牛仔褲',
-      productCount: 1,
-      productPrice: 3999,
-      totalPrice: 0,
-    },
-    {
-      id: 2,
-      img: product2,
-      productName: '刷色直筒牛仔褲',
-      productCount: 1,
-      productPrice: 1299,
-      totalPrice: 0,
-    },
-  ];
+type CartProps = {
+  products: array,
+};
 
-  const [products, setProducts] = useState(productData);
-  // const [cartPrice, setCartPrice] = useState(() => {
-  //   let price = 0;
-  //   price = products.map((product) => product.productPrice + price);
-  // });
-  // console.log(cartPrice);
-  const plusBtnAtClick = (id) => {
-    setProducts((prevProducts) => {
-      const newProducts = [];
-      for (let i = 0; i < prevProducts.length; i += 1) {
-        const currentProduct = prevProducts[i];
-        if (currentProduct.id === id) {
-          let updateProduct = {
-            ...currentProduct,
-            productCount: currentProduct.productCount + 1,
-          };
-          updateProduct = {
-            ...updateProduct,
-            totalPrice: updateProduct.productCount * updateProduct.productPrice,
-          };
-          newProducts.push(updateProduct);
-          // TODO: 算出總價
-        } else {
-          newProducts.push(currentProduct);
-        }
-      }
-      return newProducts;
-    });
-  };
+const Cart: React.FC<CartProps> = memo((props) => {
+  const { products } = props;
 
-  const minusBtnAtClick = (id) => {
-    setProducts((prevProducts) => {
-      const newProducts = [];
-      for (let i = 0; i < prevProducts.length; i += 1) {
-        const currentProduct = prevProducts[i];
-        if (currentProduct.id === id) {
-          let updateProduct = {
-            ...currentProduct,
-            productCount: currentProduct.productCount - 1,
-          };
-          updateProduct = {
-            ...updateProduct,
-            totalPrice: updateProduct.productCount * updateProduct.productPrice,
-          };
-          newProducts.push(updateProduct);
-        } else {
-          newProducts.push(currentProduct);
-        }
-      }
-      return newProducts;
-    });
-  };
-  const LineItems = products.map((product) => {
-    const { id, img, productName, productCount, productPrice, totalPrice } =
-      product;
+  const lineItems = products.map((product) => {
+    const { id, name, img, price, quantity } = product;
     return (
       <LineItem
         key={id}
         id={id}
+        name={name}
         img={img}
-        productName={productName}
-        productCount={productCount}
-        productPrice={productPrice}
-        totalPrice={totalPrice}
-        plusBtnAtClick={plusBtnAtClick}
-        minusBtnAtClick={minusBtnAtClick}
+        price={price}
+        quantity={quantity}
       />
     );
   });
-
   return (
     <section className="cart-container col col-lg-5 col-sm-12">
       <h3 className="cart-title">購物籃</h3>
 
       <section className="product-list col col-12" data-total-price="0" />
-      {LineItems}
+      {lineItems}
       <section className="cart-info shipping col col-12">
         <div className="text">運費</div>
         <div className="price" />
@@ -110,6 +38,6 @@ const Cart = () => {
       </section>
     </section>
   );
-};
+});
 
 export default Cart;
